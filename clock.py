0b7fc9b20d52a4585724974e96c9c8db6acdc5e0
nyamero0 <141497880+nyamero0@users.noreply.github.com>
2023-08-18 00:26:00 +0800

    YEP clock

---
+++ clock.py
@@ -0,0 +1,107 @@
+import math
+import turtle
+turtle.title("Clock")
+class Clock(object):
+    hourHandLength = 100
+    minHandLength = 125
+    secHandLength = 150
+    clockRadius = 200
+    lengthEnum = (hourHandLength, minHandLength, secHandLength)
+    @staticmethod
+    def cycleRatio(value : int, base : int):
+        return value % base / (base/2)
+    @staticmethod
+    def ratioToSlope(ratio : (int | float)):
+        return (
+            math.sin(math.pi * ratio),
+            math.cos(math.pi * ratio)
+        )
+    def __init__(self, hour, minute, sec):
+        self.pen = turtle.Turtle()
+        self.hourHandPen = turtle.Turtle()
+        self.minHandPen = turtle.Turtle()
+        self.secHandPen = turtle.Turtle()
+        self.handPens = {"hour":(self.hourHandPen, 12), "minute":(self.minHandPen, 60), "second":(self.secHandPen, 60)}
+        self.hour = hour
+        self.minute = minute
+        self.sec = sec
+        self.pen.screen.delay(0)
+        self.pen.speed(0)
+        self.pen.ht()
+        self.hourHandPen.pensize(6)
+        self.minHandPen.pensize(5)
+        self.secHandPen.pensize(4)
+        self.hourHandPen.pencolor("green")
+        self.minHandPen.pencolor("blue")
+        self.secHandPen.pencolor("red")
+    def draw(self):
+        self.pen.penup()
+        self.pen.sety(-Clock.clockRadius)
+        self.pen.pensize(4)
+        self.pen.pendown()
+        self.pen.circle(Clock.clockRadius)
+        self.pen.penup()
+        self.pen.home()
+        self.pen.pensize(3)
+        tempEnum = ("12", "3", "6", "9")
+        paddingEnum = [(0, -26),(-10,-14.5),(0,0),(12,-14.5)]
+        for i in range(4):
+            x, y = Clock.ratioToSlope(i / 2)
+            self.pen.penup()
+            self.pen.home()
+            self.pen.goto(x*Clock.clockRadius+paddingEnum[i][0],y*Clock.clockRadius+paddingEnum[i][1])
+            self.pen.write(tempEnum[i], False, align="center", font=("Romans", 18, "normal"))
+        self.pen.penup()
+        self.pen.home()
+        handPens_values = list(self.handPens.values())
+        tempEnum = (self.hour + self.minute / 60, self.minute, self.sec)
+        for i in range(len(handPens_values)):
+            pen, baseCycle = handPens_values[i]
+            pen.ht()    
+            pen.screen.delay(0)
+            pen.speed(0)
+            x , y = Clock.ratioToSlope(Clock.cycleRatio(tempEnum[i],baseCycle))
+            pen.goto(x * Clock.lengthEnum[i], y * Clock.lengthEnum[i])
+            pen.penup()
+            pen.home()
+        # self.pen.screen.onkey(self.tickForever,"space")
+        # self.pen.screen.listen()s
+    def clear(self):
+        self.pen.clear()
+    def updateHand(self, handName : str, value : (int, (int | float))):
+        pen, baseCycle = self.handPens[handName]
+        pen.clear()
+        pen.penup()
+        pen.home()
+        pen.pendown()
+        x, y = Clock.ratioToSlope(Clock.cycleRatio(value[0], baseCycle))
+        pen.goto(x * value[1], y * value[1])
+        pen.penup()
+        pen.home()
+        pen.pendown()
+    def clearHand(self, handName : str):
+        self.handPens[handName][0].clear()
+    # def tickForever(self):
+    #     sleep = __import__("time").sleep
+    #     hour, min, sec = self.hour + self.minute / 60, self.minute, self.sec
+    #     while True:
+    #         while sec < 60:
+    #             self.updateHand("second", (sec, Clock.secHandLength))
+    #             sleep(0.0125)
+    #             sec += 1
+    #         min += 1
+    #         hour += 1 / 60
+    #         if min >= 60:
+    #             min = 0
+    #         elif min % 5 == 0:
+    #             if hour >= 12:
+    #                 hour = 0
+    #         self.updateHand("minute", (min, Clock.minHandLength))
+    #         self.updateHand("hour",(hour,Clock.hourHandLength))
+    #         sec = 0
+def main():
+    clock = Clock(9, 15, 0)
+    clock.draw()
+    turtle.mainloop()
+if __name__ == "__main__":
+    main()
