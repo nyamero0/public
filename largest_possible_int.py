e8b06c1f6fa05f648beac3f799b55881cb61d313
nyamero0 <genesismatic0@gmail.com>
2023-09-06 15:07:41 +0800

    additional file

---
+++ largest_possible_int.py
@@ -0,0 +1,45 @@
+import re
+import sys
+def input2(text : str = "", re_pattern : str = None, err_msg : str = ""):
+    if type(re_pattern) != str:
+        return input(text)
+    temp = input(text)
+    while re.match(re_pattern, temp) == None:
+        sys.stderr.write(err_msg)
+        temp = input(text)
+    return temp
+
+# sort while the input is still string
+# and parse it as int
+def sortInt(arr,/, isReversed):
+    return int("".join(sorted(arr,reverse=isReversed)))
+
+def largestPossibleInt(arr : list):
+    return sortInt(arr, isReversed=True)
+def smallestPossibleInt(arr :list):
+    return sortInt(arr, isReversed=False)
+
+# if let's say an array/list of integer was passed we could do it this way
+# sort it by number 1 ... 9 or 9 ... 1
+# and convert it into a 10 based number
+def sortInt2(arr,/,isReversed):
+    temp = 0
+    arr = sorted(arr, reverse=isReversed)
+    for i in range(len(arr)):
+        temp += arr[i] * 10 ** i
+    return temp
+def largestPossibleInt2(arr : list):
+    return sortInt2(arr, isReversed=False)
+def smallestPossibleInt2(arr :list):
+    return sortInt2(arr, isReversed=True)
+
+
+def main():
+    arr = [input2(f"Number {i}: ","^\d$") for i in range(1, 6)]
+    print(largestPossibleInt(arr), smallestPossibleInt(arr))
+
+    arr_int = list(map(int, arr))
+    print(largestPossibleInt2(arr_int), smallestPossibleInt2(arr_int))
+
+if __name__ == "__main__":
+    main()
